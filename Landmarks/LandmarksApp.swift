4af144124e7838a1130d330c9f569a04912689f6
akrammotory <93082320+akrammotory@users.noreply.github.com>
2024-07-15 10:54:31 +0300

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Akram Husseini on 15/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
